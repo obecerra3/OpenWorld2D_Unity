7f039c5e9a0937ab8649173c7646b03dae4be60e
obecerra3 <obecerra3@gatech.edu>
2020-11-12 00:23:51 -0500

    fixed storage issue

--- Scripts/GameScreen.cs
+++ Scripts/GameScreen.cs
@@ -11,27 +11,24 @@ public class GameScreen : Event
     Player player;
     World world;
 
-    public GameScreen()
-    {
+    public GameScreen() {
         GameObject player_obj = GameObject.Find("Player");
         player = player_obj.AddComponent<Player>();
         world = new World();
     }
 
-    public override void load()
-    {
-        if (data_exists)
-        {
-            player.load();
-            world.load();
-            player.addObserver(world);
+    public override void load() {
+        if (!data_exists) {
+            saveGame(player, world);
+            loadGame();
         }
+        player.load();
+        world.load();
+        player.addObserver(world);
     }
 
-    public override void enable()
-    {
-        if (player == null && world == null)
-        {
+    public override void enable() {
+        if (player == null && world == null) {
             load();
         }
 
@@ -39,26 +36,22 @@ public class GameScreen : Event
         world.enable();
     }
 
-    public override void hide()
-    {
+    public override void hide() {
         player.hide();
         world.hide();
     }
 
-    public override void disable()
-    {
+    public override void disable() {
         saveGame(player, world);
         player.disable();
         world.disable();
     }
 
-    public override void onNotify(Notifications _notification, List<object> _data)
-    {
+    public override void onNotify(Notifications _notification, List<object> _data) {
 
     }
 
-    public override void update()
-    {
+    public override void update() {
         world.update();
     }
 
--- Scripts/Storage.cs
+++ Scripts/Storage.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
-public static class Storage
-{
+public static class Storage {
     public static int active_save = 0;
     public static Save[] saves = new Save[3];
     public static bool data_exists;
 
-    public static void saveGame(Player _player, World _world)
-    {
+    public static void saveGame(Player _player, World _world) {
         Save save = new Save(_player, _world);
         BinaryFormatter bf = new BinaryFormatter();
-        if (!Directory.Exists(Application.persistentDataPath + "/saves/"))
-        {
+        if (!Directory.Exists(Application.persistentDataPath + "/saves/")) {
             Directory.CreateDirectory(Application.persistentDataPath + "/saves/");
         }
         string save_file = "/saves/gamesave"+active_save+".save";
@@ -26,48 +23,41 @@ public static class Storage
         Debug.Log("Storage: Game Saved");
     }
 
-    public static void loadGame()
-    {
-        if (File.Exists(Application.persistentDataPath + "/saves/gamesave0.save"))
-        {
+    public static void loadGame() {
+        if (File.Exists(Application.persistentDataPath + "/saves/gamesave0.save")) {
             Debug.Log("Storage: Save Data Exists");
             BinaryFormatter bf = new BinaryFormatter();
             FileStream file = File.Open(Application.persistentDataPath + "/saves/gamesave0.save", FileMode.Open);
             saves[0] = (Save) bf.Deserialize(file);
             file.Close();
 
-            if (File.Exists(Application.persistentDataPath + "/saves/gamesave1.save"))
-            {
+            if (File.Exists(Application.persistentDataPath + "/saves/gamesave1.save")) {
                 file = File.Open(Application.persistentDataPath + "/saves/gamesave1.save", FileMode.Open);
                 saves[1] = (Save) bf.Deserialize(file);
                 file.Close();
             }
 
-            if (File.Exists(Application.persistentDataPath + "/saves/gamesave2.save"))
-            {
+            if (File.Exists(Application.persistentDataPath + "/saves/gamesave2.save")) {
                 file = File.Open(Application.persistentDataPath + "/saves/gamesave2.save", FileMode.Open);
                 saves[2] = (Save) bf.Deserialize(file);
                 file.Close();
             }
             data_exists = true;
-        } else
-        {
+        } else {
             data_exists = false;
             Debug.Log("Storage: No Save Data");
         }
 
     }
 
-    public static void deleteAllSaves()
-     {
+    public static void deleteAllSaves() {
          string path = Application.persistentDataPath + "/saves/";
          DirectoryInfo directory = new DirectoryInfo(path);
          directory.Delete(true);
          Directory.CreateDirectory(path);
      }
 
-    public static Save getActiveSave()
-    {
+    public static Save getActiveSave() {
         return saves[active_save];
     }
 
